90a96991ef42240b59d903acbc98a57a24bdb9a8
Thiago_Tassinari <thiagotassinari12@gmail.com>
2020-07-30 16:12:05 -0300

    Setup of components

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -7,11 +8,11 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
             <img className="Logo" src={Logo} alt="FullFLix Logo" />
-            </a>
+            </Link>
 
-            <Button as="a" className="BtnLink" href="/ ">
+            <Button as={Link } className="BtnLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
